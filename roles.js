d07d33bd74325830e0d834e862ae8ec4a7b4f9cc
Josh Ryan <joshua.ryan@casa.gov.au>
2020-11-06 14:00:33 +1100

    nominated personnel

---
+++ roles.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+        var hashURL = window.location.hash;
+        hashURL = hashURL.substring(1,hashURL.length);
+    var rolename;
+if (hashURL=="CEO"){
+    rolename="Chief Executive Officer";
+}
+    if (hashURL=="acc-manager"){
+    rolename="Accountable Manager";
+}
+        if (hashURL=="cont-air-man"){
+    rolename="Continuing Airworthiness Manager";
+}
+        if (hashURL=="head-of-des"){
+    rolename="Head of Design";
+}
+            if (hashURL=="head-of-opp"){
+    rolename="Head of Operations";
+}
+            if (hashURL=="res-man"){
+    rolename="Responsible Manager";
+}
+            if (hashURL=="saf-man"){
+    rolename="Safety Manager";
+}
+            if (hashURL=="trai-man"){
+    rolename="Training Manager";
+}
+    $(".role-name").text(rolename);
+   var hashlink=$(".au-btn--primary").attr("href");
+    hashlink=hashlink+"#"+hashURL;
+    
+    
+    $(".au-btn--primary").attr("href",hashlink);
+    
+    
+});// JavaScript Document
